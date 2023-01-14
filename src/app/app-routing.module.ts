0e6c6cd4b2d7c65de63b51962af8411ef5c3d8d3
mustafa jawadwala <mustafajw07@gmail.com>
2023-01-14 20:02:47 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactusPageComponent } from './pages/contactus-page/contactus-page.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '' , component : HomePageComponent , pathMatch : 'full'},
+  {path : 'products' , component : ProductPageComponent , pathMatch : 'full'},
+  {path : 'contact' , component : ContactusPageComponent  , pathMatch : 'full'},
+  {path : '**' , component : ErrorPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
+// Page
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ContactusPageComponent } from './pages/contactus-page/contactus-page.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { FooterComponent } from './partials/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ContactusPageComponent,
+    ProductPageComponent,
+    ErrorPageComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
