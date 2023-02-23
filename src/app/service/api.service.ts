f9deddb8de0c5a1fb32c89174932b296cdb85ad6
mustafa jawadwala <mustafajw07@gmail.com>
2023-02-23 18:41:27 +0530

    json and api updated

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -5,11 +5,37 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
-
+  
   constructor(private http :HttpClient) { }
 
-  // Home Page
-  getAllTrendingProducts(){
-    return this.http.get('../../assets/json/HomePageTrending.json')
+  // Get All Hepo Products
+  getAllHepoProducts(){
+    return this.http.get('../../assets/json/Hepo.json');
   }
+
+  // Get All Rehau Products
+  getAllRehauProducts(){
+    return this.http.get('../../assets/json/Rehau.json');
+  }
+
+  // Get All wicker Products
+  getAllWickerProducts(){
+    return this.http.get('../../assets/json/WickerBaskets.json');
+  }
+  
+  // Get All flora Products
+  getAllFloraProducts(){
+    return this.http.get('../../assets/json/Flora.json');
+  }
+
+  // Get All Palladium Products
+  getAllPalladiumProducts(){
+    return this.http.get('../../assets/json/Palladium.json');
+  }
+
+  // Get All Telescopic Channel
+  getAllTelescopicChannel(){
+    return this.http.get('../../assets/json/TelescopicChannel.json');
+  }
+
 }
